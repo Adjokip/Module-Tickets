e6b4b34d83d2ca9da0c2187d533a7ce77635b8dd
HMR <hmrserver@gmail.com>
2017-10-13 18:24:11 +0530

    Re-Arrange the posts
    This commit will Arrange the posts according to user's/admin's responded time..

--- modules/Tickets/include/Ticket.php
+++ modules/Tickets/include/Ticket.php
@@ -78,7 +78,7 @@ class Ticket
                             JOIN OGP_DB_PREFIXusers b
                                 ON (a.user_id = b.user_id)
                         WHERE a.ticket_id = $tid
-                        ORDER BY a.date DESC";
+                        ORDER BY a.date ASC";
 
         return $this->db->resultQuery($query) ?: array();
     }
